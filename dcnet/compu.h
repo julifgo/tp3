f22f088252b2e2b0af67c65977ca20a87c6ca100
Julian <juli?fgo@hotmail.com>
2015-06-27 18:35:44 -0300

    Agregar compu.h

---
+++ dcnet/compu.h
@@ -0,0 +1,19 @@
+/*
+/ Desc: Clase para representar una computadora
+/ Author: Grupo 17, AED2, 1C, 2015
+/
+*/
+
+#ifndef AED2_CONJ_H_
+#define AED2_CONJ_H_
+
+#include <ostream>
+#include "../aed2/TiposBasicos.h"
+#include "../aed2/Conj.h"
+
+namespace dcnet{
+	class Compu{
+
+	};
+}
+#endif
