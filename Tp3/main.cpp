019f105a32e03311ed7c504e050f594e6bcf2ef7
leamotta <lmotta@dc.uba.ar>
2015-06-27 17:42:08 -0300

    testing main

---
+++ Tp3/main.cpp
@@ -0,0 +1,22 @@
+// Tp3.cpp : Defines the entry point for the console application.
+//
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "hola mundo";
+	int variable;
+	//Lista<int> abc = Lista<int>();
+	int valor = 2;
+	//abc.AgregarAdelante(valor);
+	//int test = abc.Primero();
+	//cout << test;
+	cin >> variable;
+	return 0;
+}
+
