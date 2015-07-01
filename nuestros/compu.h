c6269689e1e3ddcfe85f870a62f91cc92862632e
Julian Francisco Gutierrez Ostrovsky <jgutierrez@ws4.labo7>
2015-07-01 20:01:18 -0300

    Pasar implementacion de compu a .cpp

--- nuestros/compu.h
+++ nuestros/compu.h
@@ -25,32 +25,13 @@ namespace dcnet{
 		estr estr;
 
 	public:
-		//Equivalente en TP a operacion Crear(ip: ip)
-		Compu(const IP ip){
-			estr.ip = ip; 
-			estr.interfaces;
-		}
-		/// Crea por copia una compu (operación Copiar())
-  		Compu(const Compu& otra){
-  			this->estr.ip = otra.estr.ip;
-  			this->estr.interfaces = Conj<Interfaz>(otra.estr.interfaces);
-  		}
-		~Compu(){
-			//Nada?
-		}
-		const IP Ip() const{
-			return this->estr.ip;
-		}
-		const Conj<Interfaz>& Interfaces() const{ //Se devuelve res como referencia a las interfaces pero no son modificables
-			return this->estr.interfaces;
-		} 
-		void AgInterfaz(const Interfaz interfaz){
-			this->estr.interfaces.AgregarRapido(interfaz);
-		}
-		/// Operacion de igualdad entre dos compus
-  		bool operator==(const Compu& otra){
-  			return this->estr.ip == otra.estr.ip;
-  		}
+		Compu(const IP ip);//Equivalente en TP a operacion Crear(ip: ip)
+		Compu(const Compu& otra);/// Crea por copia una compu (operación Copiar())
+		~Compu();
+		const IP Ip() const;
+		const Conj<Interfaz>& Interfaces() const;
+		void AgInterfaz(const Interfaz interfaz);
+		bool operator==(const Compu& otra);/// Operacion de igualdad entre dos compus
 
   	
 	};
