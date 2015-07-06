65f1c31458510a895383c3cf79fe2371d68e568c
Luis <garciagomezluis.94@gmail.com>
2015-07-06 19:34:20 -0300

    cambios en main para pruebas

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <cassert>
 #include "aed2.h"
 
-//#include "nuestros/compu.h" 
-//#include "nuestros/red.h"
-//#include "nuestros/paquete.h"
+#include "nuestros/compu.h" 
+#include "nuestros/red.h"
+#include "nuestros/paquete.h"
 #include "nuestros/ab.h"
 
-#include "nuestros/diccLog.h"
+//#include "nuestros/diccLog.h"
 
 using namespace aed2;
 
@@ -18,37 +18,41 @@ using namespace dcnet;
 int main()
 {
 
-
-	cout << "hola mundo";
-	/*int variable;
-	
+	/*
 	Lista<Nat> lista;
 	lista.AgregarAtras(2);
 	Nat primero = lista.Primero();
 	cout<<primero<<endl;
+	*/
 
+	/*
 	Dicc<Nat,String> dicc;
 	dicc.Definir(1,"Uno");
 	bool estaDef = dicc.Definido(5);
-	cout<<"Clave definida: "<<estaDef<<endl;
+	cout<<"Clave definida: "<<estaDef<<endl; */
 
-	Compu comp("192.168.50.137");
-	Compu comp2("192.168.0.1");
-	comp.AgInterfaz(0);
-	comp.AgInterfaz(36);
+	Compu comp1("0");
+	comp1.AgInterfaz(0);
+	comp1.AgInterfaz(36);
+
+	Compu comp2("1");
 	comp2.AgInterfaz(2);
-	cout<<"Ip creada "<<comp.Ip()<<endl;
-	cout<<"Interfaz creada "<<comp.Interfaces()<<endl;
-	Red r1;
-	r1.AgCompu(comp);
-	cout << "Primera Ip de Lista:" << r1.Computadoras().Primero().Ip() << endl;
-	r1.AgCompu(comp2);
-	cout<< "UsaInterfaz?:" << r1.UsaInterfaz(comp,36) << endl;
-	r1.Conectar(comp,comp2,36,2);
-	cout<< "UsaInterfaz ahora que conecte?:" << r1.UsaInterfaz(comp,36) << endl;
-	cout<< "La Ip a la que esta conectada comp:" << r1.Vecinos(comp).CrearIt().Siguiente().Ip() << endl;*/
+
+	//cout<<"Ip creada "<<comp1.Ip()<<endl;
+	//cout<<"Interfaz creada "<<comp1.Interfaces()<<endl;
+
+	Red r;
+	r.AgCompu(comp1);
+	r.AgCompu(comp2);
+	r.Conectar(comp1 ,comp2, 36, 2);
+
+	//cout << "Primera Ip de Lista:" << r1.Computadoras().Primero().Ip() << endl;
+	//cout<< "UsaInterfaz?:" << r1.UsaInterfaz(comp,36) << endl;
+	//cout<< "UsaInterfaz ahora que conecte?:" << r1.UsaInterfaz(comp,36) << endl;
+	//cout<< "La Ip a la que esta conectada comp:" << r1.Vecinos(comp).CrearIt().Siguiente().Ip() << endl;
+	//cout<<comp<<endl;
 	/*DiccLog<int,int> diccionario;
-	diccionario.Definir(1,10);*/
+	diccionario.Definir(1,10);
 	Ab<int> arbolIzq(Ab<int>(),10,Ab<int>());
 	Ab<int> arbolDer(Ab<int>(),20,Ab<int>());
 	Ab<int> arbol(arbolIzq,5,arbolDer);
@@ -58,7 +62,7 @@ int main()
 	//cout<<"cardinal arbol izquiwerdo: "<<arbol.Izq().Tamano()<<endl;
 	//cin >> variable;
 
-
+*/
 
 	/*
 
