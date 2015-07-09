2e3d65a02960b32c09eb9ac16f93c2e2849101dc
Leandro Motta <leamotta@msn.com>
2015-07-09 01:13:15 -0300

    ab y diccLog definiendo

--- nuestros/ab.h
+++ nuestros/ab.h
@@ -11,27 +11,27 @@ namespace dcnet {
 template<typename T>
 class Ab {
  private:
-    struct nodo {
+    struct Nodo {
         T valor;
         Ab<T>* _izq;
         Ab<T>* _der;
 
-        nodo(Ab<T>* i, T v, Ab<T>* d) :
+        Nodo(Ab<T>* i, T v, Ab<T>* d) :
             valor(v), _izq(i), _der(d) {}
 
-        nodo(const nodo& otro) :
+        Nodo(const Nodo& otro) :
             valor(otro.valor) {
             _izq = otro._izq == NULL ? NULL : new Ab<T>(*(otro._izq));
             _der = otro._der == NULL ? NULL : new Ab<T>(*(otro._der));
         }
         
-        ~nodo() {
+        ~Nodo() {
             delete _izq;
             delete _der;
         }
     };
     Nat _cardinal;
-    nodo* _raiz;
+    Nodo* _raiz;
 
  public:
     Ab();                          // Construye un árbol vacío
@@ -42,103 +42,103 @@ class Ab {
 
     Ab<T>& operator=(const Ab<T>&);  // Operador de asignación por copia
 
-    bool isNil() const;          // Devuelve True si y solo si el árbol
+    bool IsNil() const;          // Devuelve True si y solo si el árbol
                                     // no contiene nodos
-    T& raiz() const;             // Devuelve el valor de la raíz del árbol
-   /* Ab<T>* izq() const;          // Devuelve el subárbol izquierdo
-    Ab<T>* der() const;     */     // Devuelve el subárbol derecho
-    Ab<T>* izq();                // Devuelve el subárbol izquierdo
-    Ab<T>* der();   	           // Devuelve el subárbol derecho
-    //void izq(Ab<T>*);            // Reemplaza el subárbol izquierdo
+    T& Raiz() const;             // Devuelve el valor de la raíz del árbol
+   /* Ab<T>* Izq() const;          // Devuelve el subárbol izquierdo
+    Ab<T>* Der() const;     */     // Devuelve el subárbol derecho
+    Ab<T>* Izq();                // Devuelve el subárbol izquierdo
+    Ab<T>* Der();   	           // Devuelve el subárbol derecho
+    //void Izq(Ab<T>*);            // Reemplaza el subárbol izquierdo
                                     // (NO libera memoria)
-   // void der(Ab<T>*);            // Reemplaza el subárbol derecho
+   // void Der(Ab<T>*);            // Reemplaza el subárbol derecho
                                     // (NO libera memoria)
     Nat Tamano() const;       // Devuelve la cantidad de nodos del árbol
 };
 
 // Implementación de métodos públicos
 
-template<class T>
+template<typename T>
 Ab<T>::Ab() : _raiz(NULL), _cardinal(0) {
 }
 
-template<class T>
+template<typename T>
 Ab<T>::Ab(const Ab<T>& otro) {
-    _raiz = otro._raiz == NULL ? NULL : new nodo(*(otro._raiz));
+    _raiz = otro._raiz == NULL ? NULL : new Nodo(*(otro._raiz));
     _cardinal = otro._cardinal;
 }
 
-template<class T>
+template<typename T>
 Ab<T>::Ab(Ab<T>* i, const T& r, Ab<T>* d) {
-    _raiz = new nodo(i, r, d);
+    _raiz = new Nodo(i, r, d);
     Nat cardinalIzq = i==NULL?0:i->_cardinal;
     Nat cardinalDer = d==NULL?0:d->_cardinal;
     _cardinal = cardinalIzq + cardinalDer + 1;
 }
 
-template<class T>
+template<typename T>
 Ab<T>::~Ab() {
     delete _raiz;
 }
 
-template<class T>
+template<typename T>
 Ab<T>& Ab<T>::operator=(const Ab<T>& otro) {
     delete _raiz;
-    _raiz = otro._raiz == NULL ? NULL : new nodo(*(otro._raiz));
+    _raiz = otro._raiz == NULL ? NULL : new Nodo(*(otro._raiz));
     _cardinal = otro._cardinal;
     return *this;
 }
 
-template<class T>
-bool Ab<T>::isNil() const {
+template<typename T>
+bool Ab<T>::IsNil() const {
     return _raiz == NULL;
 }
 
-template<class T>
-T& Ab<T>::raiz() const {
-    assert(!isNil());
+template<typename T>
+T& Ab<T>::Raiz() const {
+    assert(!IsNil());
     return _raiz->valor;
 }
 
-/*template<class T>
-Ab<T>* Ab<T>::izq() const {
+/*template<typename T>
+Ab<T>* Ab<T>::Izq() const {
     assert(!esNil());
     return _raiz->_izq;
 }
 
-template<class T>
-Ab<T>* Ab<T>::der() const {
+template<typename T>
+Ab<T>* Ab<T>::Der() const {
     assert(!esNil());
     return _raiz->_der;
 }*/
 
-template<class T>
-Ab<T>* Ab<T>::izq() {
-    assert(!isNil());
+template<typename T>
+Ab<T>* Ab<T>::Izq() {
+    assert(!IsNil());
     return _raiz->_izq;
 }
 
-template<class T>
-Ab<T>* Ab<T>::der() {
-    assert(!isNil());
+template<typename T>
+Ab<T>* Ab<T>::Der() {
+    assert(!IsNil());
     return _raiz->_der;
 }
 
-/*template<class T>
-void Ab<T>::izq(Ab<T>* i) {
+/*template<typename T>
+void Ab<T>::Izq(Ab<T>* i) {
     assert(!esNil());
     // delete _raiz->_izq;
     _raiz->_izq = i;
 }
 
-template<class T>
-void Ab<T>::der(Ab<T>* d) {
+template<typename T>
+void Ab<T>::Der(Ab<T>* d) {
     assert(!esNil());
     // delete _raiz->_der;
     _raiz->_der = d;
 }*/
 
-template<class T>
+template<typename T>
 Nat Ab<T>::Tamano() const {
 return _cardinal;
 }
--- nuestros/diccLog.h
+++ nuestros/diccLog.h
@@ -21,17 +21,38 @@ namespace dcnet {
         bool IsDefinido(const K& p) const;          
                                                     
         void Definir(const K& p, const S& s){       
-            if(diccLog->isNil()){                   
+            if(diccLog->IsNil()){
                 delete diccLog;//TODO. OJO CON ESTA LINEA! Esta asi porque si estoy parado, vengo de haber sido construido sin parametros
                 Nodo *nodo = new Nodo;
                 nodo->clave = p;
                 nodo->significado = s;
-                nodo->altura =1;
+                nodo->altura = 1;
                 diccLog = new Ab<Nodo>(NULL,*nodo,NULL);
                 delete nodo;
             }
             else{
-               cout<<"No Soy nil"<<endl;   
+            	Nodo raiz = diccLog->Raiz();
+            	if(p<raiz.clave){
+            		diccLog->Izq()->Definir(p,s);
+            	}else{
+            		diccLog->Der()->Definir(p,s);
+            	}
+            	raiz.altura = 1 + (diccLog->Izq()->Raiz().altura > diccLog->Izq()->Raiz().altura)?diccLog->Izq()->Raiz().altura:diccLog->Der()->Raiz().altura;
+            	int fdb = FactorDeBalanceo();
+            	if(fdb>1 && p<diccLog->Izq()->Raiz().clave)
+            		RotarDerecha();
+            	if(fdb<-1 && p>diccLog->Der()->Raiz().clave)
+            	    RotarIzquierda();
+            	if(fdb>1 && p>diccLog->Izq()->Raiz().clave){
+            		diccLog->Izq()->RotarIzquierda();
+            		RotarDerecha();
+            	}
+            	if(fdb<-1 && p<diccLog->Der()->Raiz().clave){
+            		diccLog->Der()->RotarDerecha();
+            	    RotarIzquierda();
+            	}
+
+            	cout<<"No Soy nil"<<endl;
             }
                 
         }
@@ -43,22 +64,62 @@ namespace dcnet {
         Nat CantClaves() const;
 
         private:
-        DiccLog<K,S> RotarIzquierda(DiccLog<K,S> d); //d es subarbol de DiccLog
-        DiccLog<K,S> RotarDerecha(DiccLog<K,S> d); //d es subarbol de DiccLog
-        int FactorDeBalanceo(const DiccLog<K,S> d); //d es subarbol de DiccLog
-
         struct Nodo{
-        	K clave;
-        	S significado;
-        	Nat altura;
-                ~Nodo(){
-                    cout<<"No deberia haber entrada aca"<<endl;
-                }
+             K clave;
+             S significado;
+             Nat altura;
+             ~Nodo(){
+             cout<<"No deberia haber entrada aca"<<endl;
+             }
         };
+        Ab<Nodo>* RotarIzquierda(){
+      		Ab<Nodo>* aux1 = diccLog->der();
+        	Ab<Nodo>* aux2 = aux1->izq();
+        	aux1->izq() = diccLog;
+        	diccLog->der() = aux2;
+        	diccLog->Raiz().altura = 1 + (diccLog->izq()->Raiz().altura > diccLog->izq()->Raiz().altura)?diccLog->izq()->Raiz().altura:diccLog->der()->Raiz().altura;
+        	aux1->Raiz().altura = 1 + (aux1->izq()->Raiz().altura > aux1->izq()->Raiz().altura)?aux1->izq()->Raiz().altura:aux1->der()->Raiz().altura;
+        	return aux1;
+        };
+        Ab<Nodo>* RotarDerecha(){
+        	Ab<Nodo>* aux1 = diccLog->izq();
+        	Ab<Nodo>* aux2 = aux1->der();
+        	aux1->der() = diccLog;
+        	diccLog->izq() = aux2;
+        	diccLog->Raiz().altura = 1 + (diccLog->izq()->Raiz().altura > diccLog->izq()->Raiz().altura)?diccLog->izq()->Raiz().altura:diccLog->der()->Raiz().altura;
+        	aux1->Raiz().altura = 1 + (aux1->izq()->Raiz().altura > aux1->izq()->Raiz().altura)?aux1->izq()->Raiz().altura:aux1->der()->Raiz().altura;
+        	return aux1;
+        };
+        int FactorDeBalanceo();
+
+
 
         Ab<Nodo> *diccLog;
             
 
     };
+
+	template<typename K,typename S>
+	int DiccLog<K,S>::FactorDeBalanceo(){
+		if(diccLog->IsNil()){
+			return 0;
+		}else{
+			int alturaIzq =0;
+			int alturaDer =0;
+			if(!diccLog->izq()->IsNil()){
+				alturaIzq = diccLog->izq()->Raiz().altura;
+			}
+			if(!diccLog->Der()->IsNil()){
+				alturaDer = diccLog->Izq()->Raiz().altura;
+			}
+			return alturaIzq - alturaDer;
+		}
+	}
+
 }
+
+
+
 #endif
+
+
