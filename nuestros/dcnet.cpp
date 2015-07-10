47632ee7baed199533db42f47d52b309fc26f1bd
Julian <juli?fgo@hotmail.com>
2015-07-10 00:51:25 -0300

    Implementaciones en dcnet

--- nuestros/dcnet.cpp
+++ nuestros/dcnet.cpp
@@ -13,10 +13,26 @@ namespace dcnet{
 	DCNet::DCNet(const Red& red){
 		estr.red = &red;
 		estr.laQueMasEnvio = estr.red->Computadoras().CrearIt();
-		//estr.cantPaquetesEnviados = DiccString<Nat>(); //TODO. Revisar esto con profe.
+		//Lista<Compu>::Iterador it = estr.red->Computadoras().CrearIt();
+		/*while(it.HaySiguiente()){ //NO ANDA EL ITERADOR DE LISTA ! ! 
+			cout<<it.Siguiente().Ip()<<endl;
+			//ConjLog<Paquete> cPaq;
+			//estr.enEspera.definir(it.Siguiente().Ip(),cPaq);
+			it.Avanzar();
+		}*/
+		for (Nat i = 0; i < estr.red->Computadoras().Longitud(); i++)
+		{
+			//ConjLog<Paquete> *cPaq = new ConjLog<Paquete>();
+			estr.enEspera.definir(estr.red->Computadoras()[i].Ip(),*new ConjLog<Paquete>()); //TODO. Pierde memoria por cada creacion de un new Conj
+			Nat aux =0;
+			estr.cantPaquetesEnviados.definir(estr.red->Computadoras()[i].Ip(),aux);
+			estr.CaminoRecorrido.definir(estr.red->Computadoras()[i].Ip(),*new DiccLog<Nat,Lista<Compu>>());
+			//delete cPaq;
+		}
 	}
 	DCNet::~DCNet(){
-			//TODO. IMPLEMENTAR.
+		cout<<"destructor dcnet"<<endl;
+			
 	}
 
 	const Red& DCNet::red() const{
@@ -32,16 +48,21 @@ namespace dcnet{
 		return Lista<Compu>(); //TODO. Implementar!
 	}
 
-	const Conj<Paquete> DCNet::EnEspera(const Compu& c) const{
-		return Conj<Paquete>(); //TODO. Implementar!
+	const ConjLog<Paquete> DCNet::EnEspera(const Compu& c){
+		assert(estr.enEspera.definido(c.Ip()));
+		return *estr.enEspera.obtener(c.Ip());
 	}
 
 	void DCNet::CrearPaquete(const Paquete& p){
-
+		//ConjLog<Paquete> *cPaq = estr.enEspera.obtener(p.Origen().Ip());
+		//cPaq->Definir(p);
+		//ConjLog<int> bla;
+		//bla.Definir(1);
 	}
 
 	void DCNet::AvanzarSegundo(){
-
+		cout<<"Area test provisoria"<<endl;
+		cout<<"en espera iniciando bien: "<<estr.enEspera.definido("0")<<endl;
 	}
 
 	bool DCNet::IsPaqueteEnTransito(const Paquete& p) const{
