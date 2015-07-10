a91e45a7230f92427a2158fdd33da5805e5fa371
Julian <juli?fgo@hotmail.com>
2015-07-10 11:50:51 -0300

    Agregar conjLog.h

---
+++ nuestros/conjLog.h
@@ -0,0 +1,53 @@
+/*
+/CONJUNTO LOGARITMICO
+/ Desc: Clase para representar un conjunto con operaciones basicas en tiempo logaritmico
+/ Author: Grupo 17, AED2, 1C, 2015
+/
+*/
+#ifndef CONJLOG_H
+#define CONJLOG_H
+
+#include "../aed2.h"
+#include "diccLog.h"
+
+namespace dcnet {
+
+	template<typename T>
+	class ConjLog{
+	public:
+		ConjLog<T>(){
+			//cout<<"Creando conjLog"<<endl;
+		}
+		~ConjLog<T>(){
+
+		}
+
+		void Definir(const T& valor){
+			conj.Definir(valor,valor);
+		}
+
+		void Borrar(const T& valor){
+			if(conj.IsDefinido(valor)){
+				conj.Borrar(valor);
+			}
+		}
+
+		bool Pertenece(const T& valor) const{
+			return conj.IsDefinido(valor);
+		}
+
+		const Nat Cardinal(){
+			return conj.CantClaves();
+		}
+
+		T& Minimo() const{
+			return conj.MinimoSignificado();
+		}
+
+	private:
+		DiccLog<T,T> conj;
+	};
+
+}
+
+#endif
